7799418a35ec5acee66c0548b29412bd722f55e2
Iran Gregorio <iran.gregorio@iteris.com.br>
2020-07-01 21:57:51 -0300

    logs

--- src/ExchangeChallenge.Api/Startup.cs
+++ src/ExchangeChallenge.Api/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace ExchangeChallenge.Api
 {
@@ -37,8 +38,10 @@ namespace ExchangeChallenge.Api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddFile("logs/log{Date}.txt");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
--- src/ExchangeChallenge.Domain/Services/ExchangeService.cs
+++ src/ExchangeChallenge.Domain/Services/ExchangeService.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using ExchangeChallenge.Domain.Interfaces;
 using ExchangeChallenge.Domain.Models;
+using Microsoft.Extensions.Logging;
 
 namespace ExchangeChallenge.Domain.Services
 {
@@ -9,28 +10,37 @@ namespace ExchangeChallenge.Domain.Services
     {
         private readonly IFactorRepository _factorRepository;
         private readonly ITaxRepository _taxRepository;
+        private readonly ILogger<ExchangeService> _logger;
 
         public ExchangeService(
             IFactorRepository factorRepository,
-            ITaxRepository taxRepository)
+            ITaxRepository taxRepository,
+            ILogger<ExchangeService> logger)
         {
             _factorRepository = factorRepository;
             _taxRepository = taxRepository;
+            _logger = logger;
         }
 
         public async Task<Exchange> GetQuote(string category, string currency, decimal qty)
         {
+            _logger.LogInformation($"Calculando a conversão para {currency}. (qty = {qty})");
+
             var result = await Task.WhenAll(
                 _factorRepository.GetFactor(currency),
                 _taxRepository.GetTax(category));
             var factor = result[0];
             var tax = result[1];
+            var total = Math.Round(qty * factor * (1 + tax), 2);
+
+            _logger.LogInformation($"Resultado da conversão: {total}");
+
             return new Exchange
             {
                 ValueRequested = qty,
                 FactorApplied = factor,
                 TaxApplied = tax,
-                Total = Math.Round(qty * factor * (1 + tax), 2)
+                Total = total
             };
         }
     }
